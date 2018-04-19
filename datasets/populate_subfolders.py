a3f0e991a8eb61af4c2ec95e4f56ae7a2c310a46
David Fair <airmanfair@Davids-MacBook-Pro-5.local>
2018-04-18 22:56:53 -0700

    updated k_fold_cross_val and populate_subfolders

--- datasets/populate_subfolders.py
+++ datasets/populate_subfolders.py
@@ -4,18 +4,18 @@ root = os.path.dirname(os.path.abspath(__file__))
 raw_path = os.path.join(root, "raw")
 processed_path = os.path.join(root, "processed/adience")
 
-gender, age, gender_age = processed_path + '/gender', processed_path + '/age', processed_path + '/gender_age'
-paths = [gender, age, gender_age] + [gender + '/female', gender + '/male']
+gender, age, age_gender = processed_path + '/gender', processed_path + '/age', processed_path + '/age_gender'
+paths = [gender, age, age_gender] + [gender + '/female', gender + '/male']
 paths += [age + p for p in ['/0-2', '/4-6', '/8-13', '/15-20', '/25-32', '/38-43', '/48-53', '/60-100']]
-paths += [gender_age + p for p in ['/male_0-2', '/male_4-6', '/male_8-13', '/male_15-20', '/male_25-32', '/male_38-43', '/male_48-53', '/male_60-100']]
-paths += [gender_age + p for p in ['/female_0-2', '/female_4-6', '/female_8-13', '/female_15-20', '/female_25-32', '/female_38-43', '/female_48-53', '/female_60-100']]
+paths += [age_gender + p for p in ['/male_0-2', '/male_4-6', '/male_8-13', '/male_15-20', '/male_25-32', '/male_38-43', '/male_48-53', '/male_60-100']]
+paths += [age_gender + p for p in ['/female_0-2', '/female_4-6', '/female_8-13', '/female_15-20', '/female_25-32', '/female_38-43', '/female_48-53', '/female_60-100']]
 
 for p in paths:
     if not os.path.exists(p):
         os.makedirs(p)
 
 male, female, age_path = processed_path + '/gender/male', processed_path + '/gender/female', processed_path + '/age/'
-gender_age += '/'
+age_gender += '/'
 for j in range(5):
     folds = [np.genfromtxt(os.path.join(raw_path, "fold_%d_data.txt" % j), usecols=(0,1,2,3,4), delimiter='\t', dtype=None)]
     folds += [np.genfromtxt(os.path.join(raw_path, "fold_frontal_%d_data.txt" % j), usecols=(0,1,2,3,4), delimiter='\t', dtype=None)]
@@ -40,14 +40,14 @@ for j in range(5):
                 if age is not None:
                     for src in paths:
                         shutil.copy(src, age_path + age)
-                        shutil.copy(src, gender_age + 'male_' + age)
+                        shutil.copy(src, age_gender + 'male_' + age)
             elif gender == 'f':
                 for src in paths:
                     shutil.copy(src, female)
                 if age is not None:
                     for src in paths:
                         shutil.copy(src, age_path + age)
-                        shutil.copy(src, gender_age + 'female_' + age)
+                        shutil.copy(src, age_gender + 'female_' + age)
             elif age is not None:
                 for src in paths:
                     shutil.copy(src, age_path + age)
--- models/k_fold_cross_val.py
+++ models/k_fold_cross_val.py
@@ -1,5 +1,61 @@
-import keras
+import keras, os
 import sklearn as sk, numpy as np
 from keras.layers import Dense
 from keras.models import Sequential
 from sklearn.model_selection import StratifiedKFold
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import KFold
+
+
+
+def k_fold_cross_val(k, model, epochs, img_dims=(50, 50), batch_size=32, grayscale=True, label='age', dataset='ADIENCE'):
+    """
+    Inputs:
+    - k: number of folds
+    - model: a keras model (not pre-compiled)
+    - epochs: self explanatory
+    - img_dims: dimension of image matrices
+    - batch_size: self explanatory
+    - grayscale: self explanatory
+    - label: whether to train on age ('age') or gender ('gender)
+    - dataset: either 'IMDB', 'WIKI', or 'ADIENCE' ~ the dataset to use cross validation on
+    """
+    if dataset == 'ADIENCE':
+        path = '../datasets/processed/adience'
+    elif dataset == 'WIKI':
+        path = '../datasets/processed/wiki/'
+    elif dataset == 'IMDB':
+        path = '../datasets/processed/imdb'
+    else:
+        print("Inproper data set name!")
+    path += '/age/' if label == 'age' else '/gender/'
+
+    # train_data_dir = path + 'train/'
+    # valid_data_dir = path + 'valid/'
+
+    color_mode = 'grayscale' if grayscale else 'rgb'
+
+    dataset_datagen = ImageDataGenerator(samplewise_center=True,
+                                         samplewise_std_normalization=True,
+                                         shear_range=0.2,
+                                         zoom_range=0.2,
+                                         horizontal_flip=True,
+                                         validation_split=1.0/k)
+
+    dataset_generator = dataset_datagen.flow_from_directory(path,
+                                                            color_mode=color_mode,
+                                                            target_size=img_dims,
+                                                            batch_size=batch_size,
+                                                            class_mode='categorical')
+
+
+    valid_datagen = ImageDataGenerator(samplewise_center=True,
+                                       samplewise_std_normalization=True)
+
+    valid_generator = valid_datagen.flow_from_directory(valid_data_dir,
+                                                        color_mode=color_mode,
+                                                        target_size=img_dims,
+                                                        batch_size=batch_size,
+                                                        class_mode='categorical')
+
+    model.fit(X[train], Y[train], epochs=150, batch_size=k, verbose=0)
