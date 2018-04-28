5c295c945d477d3cb251d9d9868711c667dcdb34
David Fair <airmanfair@Davids-MacBook-Pro-5.local>
2018-04-28 15:03:29 -0700

    changed K_fold to add architecture specification

--- models/k_fold_cross_val.py
+++ models/k_fold_cross_val.py
@@ -3,13 +3,16 @@ sys.path.append('../')
 import os
 import numpy as np
 import argparse
-import models.vgg as cigaModels
+import models.vgg as vgg
+import models.resnet as resnet
+import models.AndreyNet as andreynet
 import training.trainer as cigaTraining
+import tensorflow as tf
 from keras import optimizers
+from keras import backend as K
 from testing.confusion_mtx import *
 
-
-def k_fold_cross_val(dataset, k, label_type, trainer_config, epochs=1, img_size=50, batch_size=32, grayscale=True, weights=None):
+def k_fold_cross_val(dataset, k, label_type, model_type, trainer_config, epochs=1, img_size=50, batch_size=32, grayscale=True, weights=None, architecture=None):
     num_channels = 1 if grayscale else 3
     img_shape = (img_size, img_size, num_channels)
     fold_paths = "../datasets/processed/" + dataset + "/" + label_type + "_fold_"
@@ -25,7 +28,13 @@ def k_fold_cross_val(dataset, k, label_type, trainer_config, epochs=1, img_size=
     num_classes = train.num_classes
 
     # Build k Models:
-    models = [cigaModels.getModel_vgg16(img_shape, num_classes) for _ in range(k)]
+    models = []
+    if model_type == 'vgg-16':
+        models = [vgg.getModel_vgg16(img_shape, num_classes) for _ in range(k)]
+    elif model_type == 'andreynet':
+        models = [andreynet.AndreyNet(architecture, img_shape, num_classes) for _ in range(k)]
+    elif model_type == 'resnet':
+        models = [resnet.resnetBuilder(architecture, img_shape, num_classes) for _ in range(k)]
 
     # Load pretrained weights:
     if weights:
@@ -38,7 +47,7 @@ def k_fold_cross_val(dataset, k, label_type, trainer_config, epochs=1, img_size=
 
     # Save k models:
     for i in range(k):
-        trainers[i].saveModel('VGG-16 fold %d validation accuracy ' % (i+1) + str(max(models[i].history.history['val_acc'])))
+        trainers[i].saveModel(model_type + ' fold %d validation accuracy ' % (i+1) + str(max(models[i].history.history['val_acc'])))
 
     # Generate k+1 confusion matrices (+1 for matrix across all folds):
     confusion_mtx(models, validation_generators, model_name="VGG-16", k=k)
@@ -49,12 +58,20 @@ def main():
     parser.add_argument('dataset', help='Dataset to use, either "adience", "imdb" or "wiki"')
     parser.add_argument('k', type=int, help='The number of folds to use, typically k=5 or k=7')
     parser.add_argument('label_type', help="The label type to be used, either: 'age', 'age_gender' or 'gender'")
+    parser.add_argument('model_type', help="The model type to be used, either: 'vgg-16', 'andreynet' or 'resnet'")
     parser.add_argument('-w', '--weights', help="File path to pretrained weights.")
     args = parser.parse_args()
     grayscale = True
     img_size = 224
-    batch_size = 128
-    epochs = 20
+    batch_size = 16
+    epochs = 1
+
+    # IF MODEL == RESNET || MODEL == ANDREYNET:
+    # Change values in this section for ResNet or AndreyNet architecture variable:
+    architecture, layers, stages, dense_size, dense = {}, 4, 5, 512, 1
+    architecture['stages'] = [[layers, 64 * (2 ** i)] for i in range(stages)]
+    architecture['dense'] = [dense_size for _ in range(dense)]
+
     trainer_config = {
         "reduce_lr_params": {
             "factor": 0.2
@@ -69,223 +86,26 @@ def main():
         k_fold_cross_val(args.dataset,
                          args.k,
                          args.label_type,
+                         args.model_type,
                          trainer_config,
                          epochs=epochs,
                          img_size=img_size,
                          batch_size=batch_size,
                          grayscale=grayscale,
-                         weights=args.weights)
+                         weights=args.weights,
+                         architecture=architecture)
     else:
         k_fold_cross_val(args.dataset,
                          args.k,
                          args.label_type,
+                         args.model_type,
                          trainer_config,
                          epochs=epochs,
                          img_size=img_size,
                          batch_size=batch_size,
-                         grayscale=grayscale)
+                         grayscale=grayscale,
+                         architecture=architecture)
 
 if __name__ == '__main__':
     main()
 
-# class MyDataGenerator(ImageDataGenerator):
-#     def __init__(self,
-#                  featurewise_center=False,
-#                  samplewise_center=False,
-#                  featurewise_std_normalization=False,
-#                  samplewise_std_normalization=False,
-#                  zca_whitening=False,
-#                  zca_epsilon=1e-6,
-#                  rotation_range=0.,
-#                  width_shift_range=0.,
-#                  height_shift_range=0.,
-#                  brightness_range=None,
-#                  shear_range=0.,
-#                  zoom_range=0.,
-#                  channel_shift_range=0.,
-#                  fill_mode='nearest',
-#                  cval=0.,
-#                  horizontal_flip=False,
-#                  vertical_flip=False,
-#                  rescale=None,
-#                  preprocessing_function=None,
-#                  data_format=None,
-#                  validation_split=0.0,
-#                  split=(0,0)):
-#         super(MyDataGenerator, self).__init__(self,
-#                  featurewise_center=False,
-#                  samplewise_center=False,
-#                  featurewise_std_normalization=False,
-#                  samplewise_std_normalization=False,
-#                  zca_whitening=False,
-#                  zca_epsilon=1e-6,
-#                  rotation_range=0.,
-#                  width_shift_range=0.,
-#                  height_shift_range=0.,
-#                  brightness_range=None,
-#                  shear_range=0.,
-#                  zoom_range=0.,
-#                  channel_shift_range=0.,
-#                  fill_mode='nearest',
-#                  cval=0.,
-#                  horizontal_flip=False,
-#                  vertical_flip=False,
-#                  rescale=None,
-#                  preprocessing_function=None,
-#                  data_format=None,
-#                  validation_split=0.0)
-#         self._validation_split = split
-#
-# class MyDirectoryIterator(DirectoryIterator):
-#     def __init__(self, directory, image_data_generator,
-#                  target_size=(256, 256), color_mode='rgb',
-#                  classes=None, class_mode='categorical',
-#                  batch_size=32, shuffle=True, seed=None,
-#                  data_format=None,
-#                  save_to_dir=None, save_prefix='', save_format='png',
-#                  follow_links=False,
-#                  subset=None,
-#                  interpolation='nearest'):
-#         if data_format is None:
-#             data_format = K.image_data_format()
-#         self.directory = directory
-#         self.image_data_generator = image_data_generator
-#         self.target_size = tuple(target_size)
-#         if color_mode not in {'rgb', 'grayscale'}:
-#             raise ValueError('Invalid color mode:', color_mode,
-#                              '; expected "rgb" or "grayscale".')
-#         self.color_mode = color_mode
-#         self.data_format = data_format
-#         if self.color_mode == 'rgb':
-#             if self.data_format == 'channels_last':
-#                 self.image_shape = self.target_size + (3,)
-#             else:
-#                 self.image_shape = (3,) + self.target_size
-#         else:
-#             if self.data_format == 'channels_last':
-#                 self.image_shape = self.target_size + (1,)
-#             else:
-#                 self.image_shape = (1,) + self.target_size
-#         self.classes = classes
-#         if class_mode not in {'categorical', 'binary', 'sparse',
-#                               'input', None}:
-#             raise ValueError('Invalid class_mode:', class_mode,
-#                              '; expected one of "categorical", '
-#                              '"binary", "sparse", "input"'
-#                              ' or None.')
-#         self.class_mode = class_mode
-#         self.save_to_dir = save_to_dir
-#         self.save_prefix = save_prefix
-#         self.save_format = save_format
-#         self.interpolation = interpolation
-#
-#         if subset is not None:
-#             validation_split = self.image_data_generator._validation_split
-#             if subset == 'validation':
-#                 split = validation_split
-#             elif subset == 'training':
-#                 split = (validation_split, 1)
-#             else:
-#                 raise ValueError('Invalid subset name: ', subset,
-#                                  '; expected "training" or "validation"')
-#         else:
-#             split = None
-#         self.subset = subset
-#
-#         white_list_formats = {'png', 'jpg', 'jpeg', 'bmp', 'ppm', 'tif', 'tiff'}
-#
-#         # first, count the number of samples and classes
-#         self.samples = 0
-#
-#         if not classes:
-#             classes = []
-#             for subdir in sorted(os.listdir(directory)):
-#                 if os.path.isdir(os.path.join(directory, subdir)):
-#                     classes.append(subdir)
-#         self.num_classes = len(classes)
-#         self.class_indices = dict(zip(classes, range(len(classes))))
-#
-#         pool = multiprocessing.pool.ThreadPool()
-#         function_partial = partial(_count_valid_files_in_directory,
-#                                    white_list_formats=white_list_formats,
-#                                    follow_links=follow_links,
-#                                    split=split)
-#         self.samples = sum(pool.map(function_partial,
-#                                     (os.path.join(directory, subdir)
-#                                      for subdir in classes)))
-#
-#         print('Found %d images belonging to %d classes.' % (self.samples, self.num_classes))
-#
-#         # second, build an index of the images in the different class subfolders
-#         results = []
-#
-#         self.filenames = []
-#         self.classes = np.zeros((self.samples,), dtype='int32')
-#         i = 0
-#         for dirpath in (os.path.join(directory, subdir) for subdir in classes):
-#             results.append(pool.apply_async(_list_valid_filenames_in_directory,
-#                                             (dirpath, white_list_formats, split,
-#                                              self.class_indices, follow_links)))
-#         for res in results:
-#             classes, filenames = res.get()
-#             self.classes[i:i + len(classes)] = classes
-#             self.filenames += filenames
-#             i += len(classes)
-#
-#         pool.close()
-#         pool.join()
-#         super(DirectoryIterator, self).__init__(self.samples, batch_size, shuffle, seed)
-#
-#
-#
-# def k_fold_cross_val(k, model, epochs, img_dims=(50, 50), batch_size=32, grayscale=True, label='age', dataset='ADIENCE'):
-#     """
-#     Inputs:
-#     - k: number of folds
-#     - model: a keras model (not pre-compiled)
-#     - epochs: self explanatory
-#     - img_dims: dimension of image matrices
-#     - batch_size: self explanatory
-#     - grayscale: self explanatory
-#     - label: whether to train on age ('age') or gender ('gender)
-#     - dataset: either 'IMDB', 'WIKI', or 'ADIENCE' ~ the dataset to use cross validation on
-#     """
-#     if dataset == 'ADIENCE':
-#         path = '../datasets/processed/adience'
-#     elif dataset == 'WIKI':
-#         path = '../datasets/processed/wiki/'
-#     elif dataset == 'IMDB':
-#         path = '../datasets/processed/imdb'
-#     else:
-#         print("Inproper data set name!")
-#     path += '/age/' if label == 'age' else '/gender/'
-#
-#     # train_data_dir = path + 'train/'
-#     # valid_data_dir = path + 'valid/'
-#
-#     color_mode = 'grayscale' if grayscale else 'rgb'
-#
-#     dataset_datagen = ImageDataGenerator(samplewise_center=True,
-#                                          samplewise_std_normalization=True,
-#                                          shear_range=0.2,
-#                                          zoom_range=0.2,
-#                                          horizontal_flip=True,
-#                                          validation_split=1.0/k)
-#
-#     dataset_generator = dataset_datagen.flow_from_directory(path,
-#                                                             color_mode=color_mode,
-#                                                             target_size=img_dims,
-#                                                             batch_size=batch_size,
-#                                                             class_mode='categorical')
-#
-#
-#     valid_datagen = ImageDataGenerator(samplewise_center=True,
-#                                        samplewise_std_normalization=True)
-#
-#     valid_generator = valid_datagen.flow_from_directory(valid_data_dir,
-#                                                         color_mode=color_mode,
-#                                                         target_size=img_dims,
-#                                                         batch_size=batch_size,
-#                                                         class_mode='categorical')
-#
-#     model.fit(X[train], Y[train], epochs=150, batch_size=k, verbose=0)
