c3c6b8129e5032448c35ac62570dfbf8bf1c1602
Giscard Biamby <giscard@gmail.com>
2018-05-01 16:06:26 -0700

    Updated augment_data.py to incorporage 'LOPO' logic while samping from adience

--- datasets/augment_data.py
+++ datasets/augment_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import keras
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import numpy as np
+import pandas as pd
 
 ###############################################################################
 # this script removes 200 images from the adience dataset per group for 0-2,  #
@@ -29,22 +30,34 @@ def load_all_folds_info(raw_path, age_groups):
     """
     folds = []
     for j in range(5):
-        fold = np.genfromtxt(os.path.join(raw_path, "fold_%d_data.txt" % j), usecols=(0, 1, 2, 3, 4),
-                             delimiter='\t', dtype=None)
-
+        # Faces file:
+        fold = np.genfromtxt(
+            os.path.join(raw_path, "fold_%d_data.txt" % j)
+            , usecols=(0, 1, 2, 3, 4)
+            , delimiter='\t'
+            , dtype=None
+        )[1:]
         base_dir_column = np.array([os.path.join(raw_path, "faces/faces/")] * fold.shape[0]).reshape((fold.shape[0], 1))
-        fold = np.hstack((fold, base_dir_column))
+        fold_num_column = np.array([j] * fold.shape[0]).reshape((fold.shape[0], 1))
+        fold = np.hstack((fold, base_dir_column, fold_num_column))
         folds.append(fold)
 
-        fold = np.genfromtxt(os.path.join(raw_path, "fold_frontal_%d_data.txt" % j), usecols=(0, 1, 2, 3, 4),
-                             delimiter='\t',
-                             dtype=None)
+        # Aligned file:
+        fold = np.genfromtxt(
+            os.path.join(raw_path, "fold_frontal_%d_data.txt" % j)
+            , usecols=(0, 1, 2, 3, 4)
+            , delimiter='\t'
+            , dtype=None
+        )[1:]
         base_dir_column = np.array([os.path.join(raw_path, "aligned/aligned/")] * fold.shape[0]).reshape(
             (fold.shape[0], 1))
-        fold = np.hstack((fold, base_dir_column))
+        fold_num_column = np.array([j] * fold.shape[0]).reshape((fold.shape[0], 1))
+        # print("shapes: ", fold.shape, base_dir_column.shape, fold_num_column.shape)
+        fold = np.hstack((fold, base_dir_column, fold_num_column))
         folds.append(fold)
     adience_images = np.vstack(folds)
     # print("adience_images.shape: ", adience_images.shape)
+    adience_images = add_img_counts(adience_images)
 
     # Filter some data:
     before = adience_images.shape[0]
@@ -53,17 +66,66 @@ def load_all_folds_info(raw_path, age_groups):
         age = get_age(img)
         if age is not None and age in age_groups.keys():
             keep[idx] = True
+            adience_images[idx,3] = img[3].replace("(8, 12)", "(8, 13)")
     adience_images = adience_images[keep]
     after = adience_images.shape[0]
     # print("Removed {} images on age filtering".format(after-before))
 
+    col_names = ["user_id", "filename", "face_id", "age", "gender", "base_path", "fold_num", "img_count"]
 
     adience_img_count = adience_images.shape[0]
-    return adience_images, adience_img_count
+    return adience_images, adience_img_count, col_names
+
+def add_img_counts(adience_images):
+    counts = []
+    for img in adience_images:
+        counts.append(len(get_raw_paths(img)))
+    counts_col = np.array(counts).reshape((adience_images.shape[0], 1))
+    return np.hstack((adience_images, counts_col))
+
+##
+## Accessors for adience_images np array:
+def get_userid(img):
+    return img[0]
+
+def get_filename(img):
+    return img[1]
+
+def get_face_id(img):
+    return img[2]
+
+def get_age(img):
+    age = img[3]
+    if age is not None and len(age) > 5:
+        age = age[1:-1].replace(', ', '-')
+        return age.replace("8-12", "8-13")
+    return None
+
+def get_gender(img):
+    gender = img[4]
+    return gender if len(gender) > 0 else None
+
+def get_raw_paths(img):
+    img_path = img[5]
+    img_path = os.path.join(img_path, get_userid(img))
+    img_paths = glob.glob("{}/*.{}.*{}".format(img_path, get_face_id(img), get_filename(img)))
+    return img_paths
+
+def get_fold_num(img):
+    return img[6]
+
+def get_img_count(img):
+    return img[7]
 
 
 def sample_adience_images(adience_images, num_samples, age_group):
     """
+    Picks which images to move from adience to wiki/imdb. Can't guarantee exactly num_samples,
+    the guarantee is only that at least num_samples images will be pulled from adience for the
+    specified age_group.
+
+    This is because if we pull one user's (face_id) img, we have to pull all of that user's img's, so we
+    avoid LOPO ("Leave One Person Out" issues).
 
     :param adience_images: 2d np array of img info from adience .txt files
     :param adience_indexes: list of indexes into adience_images that haven't been sampled yet
@@ -79,45 +141,34 @@ def sample_adience_images(adience_images, num_samples, age_group):
     adience_agegroup = adience_images[adience_images[:,3]==age_bucket]
     print("Total images for age_group {}: {}".format(age_group, adience_agegroup.shape[0]))
 
-    # Choose num_samples images to move:
-    adience_agegroup_indexes = list(range(adience_agegroup.shape[0]))
-    sampled_indexes_candidates = np.random.choice(adience_agegroup_indexes, size=num_samples, replace=False)
+    # All face_id's for the age_group:
+    face_ids = np.unique(adience_agegroup[:,2])
+    np.random.shuffle(face_ids)
+    # print("face_ids: ", face_ids)
+    # print("face_ids.shape: ", face_ids.shape)
 
     # Gather paths of images we will augment and move:
     sampled_images = []  # images we will augment and move
-    sampled_indexes = []
     sampled_count = 0
+    sampled_face_ids = []
 
-    for idx in sampled_indexes_candidates:
-        img = adience_images[idx]
+    for face_id in face_ids:
+        # Grab all imgs for curr. face_id:
+        imgs = adience_agegroup[adience_agegroup[:,2]==face_id]
+        print("face_id: {}, imgs: {}".format(face_id, imgs.shape))
+
         if sampled_count >= num_samples:
             break
-        img_path = os.path.join(img[-1], img[0])
+        sampled_face_ids.append(face_id)
+        for img in imgs:
+            paths = get_raw_paths(img)
+            sampled_images.append({"paths": paths, "img": img })
+            sampled_count += len(paths)
 
-        # Each image from the adience_images might have multiple coarse_tilt_aligned versions on disk,
-        # so this glob might return multiple files for one image from the images numpy array (hence the break
-        # condition at top of loop):
-        paths = glob.glob("{}/*{}".format(img_path, img[1]))
-        sampled_images.append({"paths": paths, "img": img })
-        sampled_count += len(paths)
-        sampled_indexes.append(idx)
-
-    print("age_group:'{}', #sampled_imgs: {}, #sampled_idxs: {}".format(age_group, sampled_count, len(sampled_indexes)))
+    print("age_group:'{}', #sampled_imgs: {}, #sampled_images: {}".format(age_group, sampled_count,
+                                                                           len(sampled_images)))
+    print("#face_id's sampled: {}".format(len(np.unique(sampled_face_ids))))
     return sampled_images
-
-
-def get_gender(img):
-    gender = img[4]
-    return gender if len(gender) > 0 else None
-
-
-def get_age(img):
-    age = img[3]
-    if age is not None and len(age) > 5:
-        age = age[1:-1].replace(', ', '-')
-        return age
-    return None
-
 
 def augment_img(datagen, target_dir, img, counts, train_or_valid):
     """
@@ -155,11 +206,11 @@ def augment_img(datagen, target_dir, img, counts, train_or_valid):
     if target_dir not in counts:
         counts[target_dir] = 0
 
-    for augmented in datagen.flow(img, batch_size=1, save_to_dir=target_dir, save_prefix='aug', save_format='jpeg'):
-        if aug_count >= NUM_AUG:
-            break
-        aug_count += 1
-        counts[target_dir] += 1
+    # for augmented in datagen.flow(img, batch_size=1, save_to_dir=target_dir, save_prefix='aug', save_format='jpeg'):
+    #     if aug_count >= NUM_AUG:
+    #         break
+    #     aug_count += 1
+    #     counts[target_dir] += 1
 
 
 def augment_and_move(
@@ -219,7 +270,7 @@ def augment_and_move(
     print("Deleting {} imgs from adience raw".format(len(processed)))
     for img in processed:
         if os.path.exists(img) and img.endswith(".jpg"):
-            os.remove(img)
+            # os.remove(img)
             print("Deleting ", img)
             pass
         else:
@@ -230,7 +281,7 @@ def augment_and_move(
 
 
 
-def main():
+def main_old():
     np.random.seed(57)
     datagen = ImageDataGenerator(
         rotation_range=15.0,
@@ -247,22 +298,23 @@ def main():
 
     gender, age, age_gender = target_path + '/gender', target_path + '/age', target_path + '/age_gender'
     paths = [gender, age, age_gender] + [gender + '/female', gender + '/male']
-    paths += [age + p for p in ['/0-2', '/4-6', '/8-12', '/15-20', '/25-32', '/38-43', '/48-53', '/60-130']]
-    paths += [age_gender + p for p in ['/male_0-2', '/male_4-6', '/male_8-12', '/male_15-20', '/male_25-32',
-                                       '/male_38-43', '/male_48-53', '/male_60-130']]
-    paths += [age_gender + p for p in ['/female_0-2', '/female_4-6', '/female_8-12', '/female_15-20',
-                                       '/female_25-32', '/female_38-43', '/female_48-53', '/female_60-130']]
+    paths += [age + p for p in ['/a_0-2', '/b_4-6', '/c_8-12', '/d_15-20', '/e_25-32', '/f_38-43', '/g_48-53',
+                                '/h_60-130']]
+    paths += [age_gender + p for p in ['/i_male_0-2', '/j_male_4-6', '/k_male_8-13', '/l_male_15-20', '/m_male_25-32',
+                                       '/n_male_38-43', '/o_male_48-53', '/p_male_60-130']]
+    paths += [age_gender + p for p in ['/a_female_0-2', '/b_female_4-6', '/c_female_8-12', '/d_female_15-20',
+                                       '/e_female_25-32', '/f_female_38-43', '/g_female_48-53', '/h_female_60-130']]
 
     male, female, age_path = target_path + '/gender/male', target_path + '/gender/female', target_path + '/age/'
     print("processed_path: ", target_path)
     print("male, female, age_path : ", male, female, age_path )
 
-    age_groups = {"0-2":0, "4-6":0, "8-12":0, "15-20":0}
+    age_groups = {"0-2":0, "4-6":0, "8-13":0, "15-20":0}
     # how many samples to move from adience, for each age_group:
     NUM_SAMPLES = 200
 
     # Load all the adience image info into a single numpy matrix:
-    adience_images, adience_img_count = load_all_folds_info(raw_path, age_groups)
+    adience_images, adience_img_count, col_names = load_all_folds_info(raw_path, age_groups)
 
 
     # For each age_group, sample images from adience, and move to wiki:
@@ -287,7 +339,31 @@ def main():
     print("Total Deleted from adience: ", num_deleted)
     print("Done! ")
 
-main()
+
+def to_df(arr, cols):
+    data = {col_name:arr[:,i] for i,col_name in enumerate(cols)}
+    df = pd.DataFrame(data)
+    return df
+
+def main():
+    root = os.path.dirname(os.path.abspath(__file__))
+    raw_path = os.path.join(root, "raw")
+    age_groups = {"0-2":0, "4-6":0, "8-13":0, "15-20":0}
+    adience_images, adience_img_count, col_names = load_all_folds_info(raw_path, age_groups)
+    df = to_df(adience_images, col_names)
+
+    df = df[df['age'].isin(
+        ["(0, 2)", "(4, 6)", "(8, 12)", "(15, 20)", "(25, 32)", "(38, 43)", "(48, 53)", "(60, 100)"]
+    )]
+    print(df.sort_values(by=["age", "fold_num", "face_id"]).groupby(["age", "fold_num", "face_id"]).img_count.sum())
+
+
+
+
+
+## =====================================================================================================================
+# main()
+main_old()
 
 # wiki before: 19449
 # wiki after: 20131
