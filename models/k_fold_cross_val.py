02fd0215367e315c4b3abad3fe7f34dfab7d8058
Giscard Biamby <giscard@gmail.com>
2018-05-07 11:53:28 -0700

    Re-running k-folds on vgg, and for resnet gender

--- models/k_fold_cross_val.py
+++ models/k_fold_cross_val.py
@@ -213,18 +213,18 @@ def main_2():
         #         "optimizer_params": {"lr": 0.00015}, "batch_size": 64, "stages": 4, "layers": 2, "dense": 2,
         #         "dense_size": 256, "img_size": 224, "grayscale": True
         #    }, None)
-        (
-            "kfold2_sameres_age_gender"
-            , "age_gender"
-            , "sameres"
-            , "./saved_models/converge2_sameres_age_gender-val_acc-0.437812-Thu_03_May_2018_04_46_57/converge2_sameres_age_gender-val_acc-0.437812-Thu_03_May_2018_04_46_57.h5"
-            , {
-                "reduce_lr_params": {"factor": 0.2, "patience": 3, "min_lr": 1e-08, "verbose": True}, "optimizer":
-                "adam", "optimizer_params": {"lr": 0.001}, "batch_size": 256, "stages": 3, "layers": 2,
-                "dense": 1, "dense_size": 512, "img_size": 224, "grayscale": True
-            }
-            , None
-         )
+        # (
+        #     "kfold2_sameres_age_gender"
+        #     , "age_gender"
+        #     , "sameres"
+        #     , "./saved_models/converge2_sameres_age_gender-val_acc-0.437812-Thu_03_May_2018_04_46_57/converge2_sameres_age_gender-val_acc-0.437812-Thu_03_May_2018_04_46_57.h5"
+        #     , {
+        #         "reduce_lr_params": {"factor": 0.2, "patience": 3, "min_lr": 1e-08, "verbose": True}, "optimizer":
+        #         "adam", "optimizer_params": {"lr": 0.001}, "batch_size": 256, "stages": 3, "layers": 2,
+        #         "dense": 1, "dense_size": 512, "img_size": 224, "grayscale": True
+        #     }
+        #     , None
+        #  )
 
         ## RESNET
         # ("kfold2_resnet_age", "age", "resnet"
@@ -234,44 +234,58 @@ def main_2():
         #                   "optimizer_params": {"lr": 0.0001}, "batch_size": 128, "stages": 3, "layers": 3,
         #        "dense": 2, "dense_size": 512, "img_size": 224, "grayscale": True
         #    }, None)
+        (
+            "kfold2_resnet_gender"
+            , "gender"
+            , "resnet"
+           , "./saved_models/converge2_resnet_gender-val_acc-0.880383-Thu_03_May_2018_20_19_38/converge2_resnet_gender-val_acc-0.880383-Thu_03_May_2018_20_19_38.h5"
+           , {
+               "reduce_lr_params": {"factor": 0.2, "patience": 3, "min_lr": 1e-08}, "optimizer": "adam",
+               "optimizer_params": {"lr": 0.001}, "batch_size": 128, "stages": 2, "layers": 1, "dense": 3,
+               "dense_size": 512, "img_size": 224, "grayscale": True
+           }
+            , None
+        )
         # , (
-        #     "kfold2_resnet_gender"
-        #     , "gender"
+        #     "kfold2_resnet_age_gender"
+        #     , "age_gender"
         #     , "resnet"
-        #    , "./saved_models/converge_resnet_gender-val_acc-0.910389-Sun_29_Apr_2018_10_45_04/converge_resnet_gender-val_acc-0.910389-Sun_29_Apr_2018_10_45_04.h5"
-        #    , {
-        #        "reduce_lr_params": {"factor": 0.2, "patience": 3, "min_lr": 1e-08}, "optimizer": "adam",
-        #        "optimizer_params": {"lr": 0.001}, "batch_size": 128, "stages": 2, "layers": 1, "dense": 3,
-        #        "dense_size": 512, "img_size": 224, "grayscale": True
-        #    }
+        #     , "./saved_models/converge2_resnet_age_gender-val_acc-0.459219-Fri_04_May_2018_02_26_57/converge2_resnet_age_gender-val_acc-0.459219-Fri_04_May_2018_02_26_57.h5"
+        #     , {
+        #         "reduce_lr_params": {"factor": 0.2, "patience": 3, "min_lr": 1e-08, "verbose": True}, "optimizer":
+        #         "adam", "optimizer_params": {"lr": 0.001}, "batch_size": 64, "stages": 2, "layers": 3, "dense": 2,
+        #         "dense_size": 128, "img_size": 224, "grayscale": True
+        #     }
         #     , None
         # )
-        , (
-            "kfold2_resnet_age_gender"
-            , "age_gender"
-            , "resnet"
-            , "./saved_models/converge2_resnet_age_gender-val_acc-0.459219-Fri_04_May_2018_02_26_57/converge2_resnet_age_gender-val_acc-0.459219-Fri_04_May_2018_02_26_57.h5"
-            , {
-                "reduce_lr_params": {"factor": 0.2, "patience": 3, "min_lr": 1e-08, "verbose": True}, "optimizer":
-                "adam", "optimizer_params": {"lr": 0.001}, "batch_size": 64, "stages": 2, "layers": 3, "dense": 2,
-                "dense_size": 128, "img_size": 224, "grayscale": True
-            }
-            , None
-        )
 
         ## VGG
-        , (
-            "kfold2_vgg16_age"
-            , "age"
-            , "vgg-16"
-            , "./saved_models/converge2_vgg16_age-val_acc-0.476328-Fri_04_May_2018_04_11_44/converge2_vgg16_age-val_acc-0.476328-Fri_04_May_2018_04_11_44.h5"
-            , {
-                "reduce_lr_params": {"factor": 0.1, "patience": 3, "min_lr": 1e-08, "verbose": True}, "optimizer":
-                "sgd", "optimizer_params": {"lr": 0.1, "decay": 0.005, "momentum": 0.85001, "nesterov": True},
-                "batch_size": 80, "img_size": 224, "grayscale": True, "early_stop_patience": 7
-            }
-            , None
-        )
+        # (
+        #     "kfold2_vgg16_age"
+        #     , "age"
+        #     , "vgg-16"
+        #     , "./saved_models/converge2_vgg16_age-val_acc-0.476328-Fri_04_May_2018_04_11_44/converge2_vgg16_age-val_acc-0.476328-Fri_04_May_2018_04_11_44.h5"
+        #     , {
+        #       "reduce_lr_params": {
+        #         "factor": 0.1,
+        #         "patience": 3,
+        #         "min_lr": 1E-8,
+        #         "verbose": True
+        #       },
+        #       "optimizer": "sgd",
+        #       "optimizer_params": {
+        #         "lr": 0.001,
+        #         "decay": 0.005,
+        #         "momentum": 0.85,
+        #         "nesterov": True
+        #       },
+        #       "batch_size": 100,
+        #       "img_size": 224,
+        #       "grayscale": True,
+        #       "early_stop_patience": 6
+        #     }
+        #     , None
+        # )
         , (
             "kfold2_vgg16_gender"
             , "gender"
@@ -309,7 +323,7 @@ def main_2():
                 "early_stop_patience": 7
             }
             , None
-            )
+        )
     ]
 
     grayscale = True
@@ -318,6 +332,7 @@ def main_2():
     K = 5
 
     for model_name, label_type, model_type, weights_path, trainer_config, folds_results in model_configs:
+        architecture = {}
         if "stages" in trainer_config:
             # (IF MODEL == RESNET || MODEL == ANDREYNET) && (not using pre-trained model):
             # Change values in this section for ResNet or AndreyNet architecture variable:
